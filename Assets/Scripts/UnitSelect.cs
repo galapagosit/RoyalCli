548049ad2ab164ddb89018e29de4b3d02a54a3a9
Daisuke Hashimoto <galapagosit@gmail.com>
2017-05-05 10:33:39 +0900

    select unit

--- Assets/Scripts/UnitSelect.cs
+++ Assets/Scripts/UnitSelect.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class UnitSelect : MonoBehaviour {
 	
 	public void Selected () {
-		Debug.Log (gameObject);
+		var btns = GameObject.FindGameObjectsWithTag ("unit_select_button");
+		foreach (var btn in btns)
+		{
+			var rt = btn.GetComponent<RectTransform> ();
+			var tmp_pos = rt.localPosition;
+			if (btn == gameObject) {
+				tmp_pos.y = -810f;
+			} else {
+				tmp_pos.y = -830f;
+			}
+			rt.localPosition = tmp_pos;
+		}
 	}
 }
